36ce0e57caaa8d4eea3a64c9cffd33c2155445da
Blake Anderson <blanderson1919@outlook.com>
2026-01-29 15:44:18 -0800

    Added place holder shop panel and navigation buttons

--- Assets/Scripts/MenuNavigationButtons.cs
+++ Assets/Scripts/MenuNavigationButtons.cs
@@ -8,52 +8,85 @@ public class MenuNavigationButtons : MonoBehaviour
     [SerializeField] private Button settingsScreenButton;
     [SerializeField] private Button startScreenButton;
     [SerializeField] private Button gameScreenButton;
+    [SerializeField] private Button shopScreenButton;
+    [SerializeField] private Button backFromShopButton;
     [SerializeField] private AudioClip buttonSoundClip;
 
     public GameObject startScreenPanel;
     public GameObject gameScreenPanel;
     public GameObject settingsScreenPanel;
-
+    public GameObject shopScreenPanel;
     public GameObject checkpointSpawner;
+
     private CheckpointSpawnScript checkpointSpawnScript;
 
-    private void Awake() {
+    private void Awake()
+    {
         //enables all buttons that have been connected
-        if (settingsScreenButton != null) {
+        if (settingsScreenButton != null)
+        {
             settingsScreenButton.onClick.AddListener(() => SettingsScreenButtonAction());
         }
-        if (startScreenButton) {
+        if (startScreenButton)
+        {
             startScreenButton.onClick.AddListener(() => StartScreenButtonAction());
         }
-        if (gameScreenButton != null) {
+        if (gameScreenButton != null)
+        {
             gameScreenButton.onClick.AddListener(() => GameScreenButtonAction());
+        }
+        if (shopScreenButton != null)
+        {
+            shopScreenButton.onClick.AddListener(() => ShopScreenButtonAction());
+        }
+        if (backFromShopButton != null)
+        {
+            backFromShopButton.onClick.AddListener(() => BackFromShopAction());
         }
     }
 
-    void Start(){
+    void Start()
+    {
         checkpointSpawnScript = checkpointSpawner.GetComponent<CheckpointSpawnScript>();
-        
+
         gameScreenPanel.SetActive(false);
         settingsScreenPanel.SetActive(false);
+        shopScreenPanel.SetActive(false);
     }
 
-    private void StartScreenButtonAction(){
+    private void StartScreenButtonAction()
+    {
         SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
         SwitchPanels(settingsScreenPanel, startScreenPanel);
     }
 
-    private void GameScreenButtonAction(){
+    private void GameScreenButtonAction()
+    {
         SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
         SwitchPanels(startScreenPanel, gameScreenPanel);
         GameStateManager.Instance.StartPlaying();
     }
 
-    private void SettingsScreenButtonAction(){
+    private void SettingsScreenButtonAction()
+    {
         SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
         SwitchPanels(startScreenPanel, settingsScreenPanel);
     }
 
-    private void SwitchPanels(GameObject srcPanel, GameObject dstPanel){
+    private void ShopScreenButtonAction()
+    {
+        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        SwitchPanels(startScreenPanel, shopScreenPanel);
+    }
+
+    private void BackFromShopAction()
+    {
+        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        SwitchPanels(shopScreenPanel, startScreenPanel);
+    }
+
+    private void SwitchPanels(GameObject srcPanel, GameObject dstPanel)
+    {
         srcPanel.SetActive(false);
         dstPanel.SetActive(true);
     }

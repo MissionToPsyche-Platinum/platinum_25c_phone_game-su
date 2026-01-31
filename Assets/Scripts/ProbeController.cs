983f17aa46aa31f2eaa9c322e7c446e643743498
ayim-en <ayimen.dev@gmail.com>
2026-01-31 10:43:46 -0800

    Update ProbeController.cs

--- Assets/Scripts/ProbeController.cs
+++ Assets/Scripts/ProbeController.cs
@@ -8,9 +8,12 @@ public class ProbeController : MonoBehaviour
     private Rigidbody2D myRigidbody2D;
     private ProbeHealth healthSystem;
     private PowerUpBehavior powerUpSystem;
+    private Vector2 minAllowedPosition;
+    private Vector2 maxAllowedPosition;
 
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private AudioClip asteroidCollisionSoundClip;
+    [SerializeField] private float extraPadding = 0.1f; // Adjust value to keep entire probe from going offscreen
 
     public event EventHandler<EventArgs> OnTakeDamage;
 
@@ -29,7 +32,35 @@ public class ProbeController : MonoBehaviour
     private void Start()
     {
         this.gameObject.SetActive(false);
-        GameStateManager.Instance.OnStartPlaying += OnStartPlaying;
+        GameStateManager.Instance.OnStartPlaying += OnStartPlaying; 
+        CalculateScreenBoundaries(); 
+    }
+    
+    // Calculates padding to add to keep probe fully on screen
+    private void CalculateScreenBoundaries()
+    {
+        Camera cameraMain = Camera.main;
+        // Viewport provides coordinates with (0,0) being bottom left and (1,1) being top right
+        Vector2 minScreen = cameraMain.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector2 maxScreen = cameraMain.ViewportToWorldPoint(new Vector3(1, 1, 0));
+
+        // Getting the component provides us the width of the probe, if not default to probe being 1f wide
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        float playerHalfWidth = 0.5f; 
+        float playerHalfHeight = 0.5f;
+
+        if (spriteRenderer != null)
+        {
+            playerHalfWidth = spriteRenderer.bounds.extents.x;
+            playerHalfHeight = spriteRenderer.bounds.extents.y;
+        }
+        
+        // Adding half the player width ensures the entire probe remains on screen when colliding with the edge
+        minAllowedPosition.x = minScreen.x + playerHalfWidth + extraPadding;
+        minAllowedPosition.y = minScreen.y + playerHalfHeight + extraPadding;
+
+        maxAllowedPosition.x = maxScreen.x - playerHalfWidth - extraPadding;
+        maxAllowedPosition.y = maxScreen.y - playerHalfHeight - extraPadding;
     }
 
     private void OnDestroy()
@@ -108,6 +139,18 @@ public class ProbeController : MonoBehaviour
         
         // Debug.Log("Movement vector: " +  myInputActions.Player.Movement.ReadValue<Vector2>());
         myRigidbody2D.linearVelocity = myInputActions.Player.Movement.ReadValue<Vector2>() * moveSpeed;
+    }
+    
+    // Used here so this happens at the very end of a frame as to not mess up linearVelocity calculation
+    private void LateUpdate() 
+    {
+        Vector3 viewPos = transform.position;
+        
+        // Clamp ensures the probe is within an allowed postion, if not it sets the value to either min or max
+        viewPos.x = Mathf.Clamp(viewPos.x, minAllowedPosition.x, maxAllowedPosition.x);
+        viewPos.y = Mathf.Clamp(viewPos.y, minAllowedPosition.y, maxAllowedPosition.y);
+
+        transform.position = viewPos;
     }
 
     private void OnStartPlaying(object sender, EventArgs e)

b793660797b7da40c24adcc53c44874039a7fb2d
Aidan <aidanlyum808@gmail.com>
2026-02-04 11:47:05 -0800

    Fixed bug where input would be disabled when screen cracks
    The visual overlay for the screen cracking would block raycasts. I fixed this by adding a canvas group to that overlay, and unchecking "Block raycasts"

--- Assets/Scripts/ComponentDisablers/EngineComponent.cs
+++ Assets/Scripts/ComponentDisablers/EngineComponent.cs
@@ -1,13 +1,22 @@
+using System;
 using UnityEngine;
 
 public class EngineComponent : ProbeComponent
 {
     [SerializeField] private ScoreIncrement myScoreIncrement;
     [SerializeField] private AudioClip engineBreakSoundEffect;
-    
+    [SerializeField] private GameObject smokeEffect;
+
+
+    private void Awake()
+    {
+        smokeEffect.gameObject.SetActive(false);
+    }
+
     protected override void DisableComponent()
     {
         base.DisableComponent();
+        smokeEffect.gameObject.SetActive(true);
         myScoreIncrement.SetScoreMultiplier(0.5f);
         SFXController.instance.PlaySoundFXClip(engineBreakSoundEffect, this.transform, 1f);
         Debug.Log("Engine component disabled! Score increases slower");
@@ -16,6 +25,7 @@ public class EngineComponent : ProbeComponent
     protected override void RepairComponent()
     {
         base.RepairComponent();
+        smokeEffect.gameObject.SetActive(false);
         myScoreIncrement.SetScoreMultiplier(1f);
         Debug.Log("Engine component enabled! Score increases normally");
     }

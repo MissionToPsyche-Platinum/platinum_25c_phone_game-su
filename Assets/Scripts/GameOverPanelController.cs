5666a98df6a36a5bebe3a5efe82a8e6d27c60869
ayim-en <ayimen.dev@gmail.com>
2026-02-10 13:26:24 -0800

    feat: hooked up buttons to navigate to proper locations and made restart and quit do respective functions

--- Assets/Scripts/GameOverPanelController.cs
+++ Assets/Scripts/GameOverPanelController.cs
@@ -1,27 +1,60 @@
 using System;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverPanelController : MonoBehaviour
 {
     [SerializeField] private Button restartGameButton;
+    [SerializeField] private Button shopButton;
     [SerializeField] private GameObject gameOverPanel;
 
     private void Awake()
     {
-        restartGameButton.onClick.AddListener(RestartGameButtonClicked);
-        gameOverPanel.SetActive(false);
+        if (restartGameButton != null)
+            restartGameButton.onClick.AddListener(RestartGameButtonClicked);
+
+        if (shopButton != null)
+            shopButton.onClick.AddListener(ShopButtonClicked);
+
+        // Ensure the visual panel is hidden at the start
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+        
+        // Also ensure the main object is hidden if this script is on the root
+        this.gameObject.SetActive(false);
     }
 
     public void RestartGameButtonClicked()
     {
-        gameOverPanel.SetActive(false);
-        GameStateManager.Instance.StartPlaying();
+        // Reloads the scene to reset everything (consistent with your Pause Menu Restart)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ShopButtonClicked()
+    {
+        this.gameObject.SetActive(false);
+        if (gameOverPanel != null) gameOverPanel.SetActive(false);
+        
+        MenuNavigationButtons menuNav = FindFirstObjectByType<MenuNavigationButtons>();
+        
+        if (menuNav != null)
+        {
+            // Hide the game screen (since we are leaving the game)
+            if (menuNav.gameScreenPanel != null) 
+                menuNav.gameScreenPanel.SetActive(false);
+
+            // Open the Shop
+            if (menuNav.shopScreenPanel != null) 
+                menuNav.shopScreenPanel.SetActive(true);
+        }
     }
 
     public void Show()
     {
         this.gameObject.SetActive(true);
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
     }
 }
--- Assets/Scripts/MenuNavigationButtons.cs
+++ Assets/Scripts/MenuNavigationButtons.cs
@@ -1,117 +1,242 @@
-using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class MenuNavigationButtons : MonoBehaviour
 {
+    [Header("Main Menu Buttons")]
     [SerializeField] private Button settingsScreenButton;
-    [SerializeField] private Button startScreenButton;
+    [SerializeField] private Button startScreenButton; 
     [SerializeField] private Button gameScreenButton;
     [SerializeField] private Button shopScreenButton;
     [SerializeField] private Button statsScreenButton;
     [SerializeField] private Button backFromShopButton;
     [SerializeField] private Button backFromStatsButton;
+    [SerializeField] private Button backFromSettingsButton;
+    
+    [Header("Pause Menu Buttons")]
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private Button restartButton; // Added Restart Button
+
+    [Header("Audio")]
     [SerializeField] private AudioClip buttonSoundClip;
 
+    [Header("Panels")]
     public GameObject startScreenPanel;
     public GameObject gameScreenPanel;
     public GameObject settingsScreenPanel;
     public GameObject shopScreenPanel;
     public GameObject statsScreenPanel;
+    public GameObject pauseMenuPanel;
     public GameObject checkpointSpawner;
 
     private CheckpointSpawnScript checkpointSpawnScript;
+    private PlayerInput playerInput;
+    private bool isPaused = false;
 
     private void Awake()
     {
-        //enables all buttons that have been connected
         if (settingsScreenButton != null)
-        {
             settingsScreenButton.onClick.AddListener(() => SettingsScreenButtonAction());
-        }
+        
         if (startScreenButton)
-        {
             startScreenButton.onClick.AddListener(() => StartScreenButtonAction());
-        }
+        
         if (gameScreenButton != null)
-        {
             gameScreenButton.onClick.AddListener(() => GameScreenButtonAction());
-        }
+        
         if (shopScreenButton != null)
-        {
             shopScreenButton.onClick.AddListener(() => ShopScreenButtonAction());
-        }
+        
         if (backFromShopButton != null)
-        {
             backFromShopButton.onClick.AddListener(() => BackFromShopAction());
-        }
+        
         if (statsScreenButton != null)
-        {
             statsScreenButton.onClick.AddListener((() => StatsScreenButtonAction()));
-        }
 
         if (backFromStatsButton != null)
-        {
             backFromStatsButton.onClick.AddListener((() => BackFromStatsAction()));
-        }
+
+        if (backFromSettingsButton != null)
+            backFromSettingsButton.onClick.AddListener(() => BackFromSettingsAction());
+
+        if (pauseButton != null)
+            pauseButton.onClick.AddListener(PauseGame);
+
+        if (resumeButton != null)
+            resumeButton.onClick.AddListener(ResumeGame);
+
+        if (quitButton != null)
+            quitButton.onClick.AddListener(QuitGame);
+
+        // Hook up Restart to do the exact same thing as Quit
+        if (restartButton != null)
+            restartButton.onClick.AddListener(QuitGame);
     }
 
     void Start()
     {
-        checkpointSpawnScript = checkpointSpawner.GetComponent<CheckpointSpawnScript>();
+        if (checkpointSpawner != null)
+            checkpointSpawnScript = checkpointSpawner.GetComponent<CheckpointSpawnScript>();
 
-        gameScreenPanel.SetActive(false);
-        settingsScreenPanel.SetActive(false);
-        shopScreenPanel.SetActive(false);
+        if (startScreenPanel != null) startScreenPanel.SetActive(true);
+        if (gameScreenPanel != null) gameScreenPanel.SetActive(false);
+        if (settingsScreenPanel != null) settingsScreenPanel.SetActive(false);
+        if (shopScreenPanel != null) shopScreenPanel.SetActive(false);
+        if (statsScreenPanel != null) statsScreenPanel.SetActive(false);
+        if (pauseMenuPanel != null) pauseMenuPanel.SetActive(false);
+
+        SetMainMenuButtonsActive(true);
+
+        if (pauseButton != null) pauseButton.gameObject.SetActive(false);
+
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        playerInput = FindFirstObjectByType<PlayerInput>();
+    }
+
+    private void SetMainMenuButtonsActive(bool isActive)
+    {
+        if (gameScreenButton != null) gameScreenButton.gameObject.SetActive(isActive);
+        if (settingsScreenButton != null) settingsScreenButton.gameObject.SetActive(isActive);
+        if (shopScreenButton != null) shopScreenButton.gameObject.SetActive(isActive);
+        if (statsScreenButton != null) statsScreenButton.gameObject.SetActive(isActive);
+    }
+
+    private void GameScreenButtonAction()
+    {
+        if (startScreenPanel != null) startScreenPanel.SetActive(false);
+        
+        SetMainMenuButtonsActive(false);
+
+        if (gameScreenPanel != null) gameScreenPanel.SetActive(true);
+
+        PlayButtonSound();
+
+        if (GameStateManager.Instance != null)
+        {
+            GameStateManager.Instance.StartPlaying();
+        }
+        
+        if (pauseButton != null) pauseButton.gameObject.SetActive(true);
+    }
+
+    private void PlayButtonSound()
+    {
+        if (SFXController.instance != null && buttonSoundClip != null)
+        {
+            SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        }
+    }
+
+    private void PauseGame()
+    {
+        PlayButtonSound();
+
+        isPaused = true;
+        Time.timeScale = 0f;
+
+        if (pauseMenuPanel != null)
+            pauseMenuPanel.SetActive(true);
+
+        if (playerInput != null)
+            playerInput.DeactivateInput();
+    }
+
+    private void ResumeGame()
+    {
+        PlayButtonSound();
+
+        isPaused = false;
+        Time.timeScale = 1f;
+
+        if (pauseMenuPanel != null)
+            pauseMenuPanel.SetActive(false);
+        
+        if (playerInput != null)
+            playerInput.ActivateInput();
+    }
+
+    // This method now handles both Quit and Restart buttons
+    private void QuitGame()
+    {
+        PlayButtonSound();
+
+        Time.timeScale = 1f;
+
+        if (playerInput != null)
+            playerInput.ActivateInput();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
     }
 
     private void StatsScreenButtonAction()
     {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        PlayButtonSound();
+        SetMainMenuButtonsActive(false);
         SwitchPanels(startScreenPanel, statsScreenPanel);
     }
 
     private void BackFromStatsAction()
     {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        PlayButtonSound();
         SwitchPanels(statsScreenPanel, startScreenPanel);
+        SetMainMenuButtonsActive(true);
     }
     
     private void StartScreenButtonAction()
     {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        PlayButtonSound();
         SwitchPanels(settingsScreenPanel, startScreenPanel);
-    }
-
-    private void GameScreenButtonAction()
-    {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
-        SwitchPanels(startScreenPanel, gameScreenPanel);
-        GameStateManager.Instance.StartPlaying();
+        SetMainMenuButtonsActive(true);
     }
 
     private void SettingsScreenButtonAction()
     {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        PlayButtonSound();
+        SetMainMenuButtonsActive(false);
         SwitchPanels(startScreenPanel, settingsScreenPanel);
+    }
+
+    private void BackFromSettingsAction()
+    {
+        PlayButtonSound();
+        SwitchPanels(settingsScreenPanel, startScreenPanel);
+        SetMainMenuButtonsActive(true);
     }
 
     private void ShopScreenButtonAction()
     {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        PlayButtonSound();
+        SetMainMenuButtonsActive(false);
         SwitchPanels(startScreenPanel, shopScreenPanel);
     }
 
     private void BackFromShopAction()
     {
-        SFXController.instance.PlaySoundFXClip(buttonSoundClip, transform, 1f);
+        PlayButtonSound();
         SwitchPanels(shopScreenPanel, startScreenPanel);
+        SetMainMenuButtonsActive(true);
     }
 
     private void SwitchPanels(GameObject srcPanel, GameObject dstPanel)
     {
-        srcPanel.SetActive(false);
-        dstPanel.SetActive(true);
+        if(srcPanel != null) srcPanel.SetActive(false);
+        if(dstPanel != null) dstPanel.SetActive(true);
     }
 }

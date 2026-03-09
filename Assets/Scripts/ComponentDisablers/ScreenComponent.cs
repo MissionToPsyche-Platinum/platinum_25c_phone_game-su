bc1745f679445db82fc7f96d0a5de082a5bcc5cd
Aidan <aidanlyum808@gmail.com>
2026-03-08 17:55:24 -0700

    Added a fade out to the cracked screen image

--- Assets/Scripts/ComponentDisablers/ScreenComponent.cs
+++ Assets/Scripts/ComponentDisablers/ScreenComponent.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenComponent : ProbeComponent
 {
     [SerializeField] private GameObject crackedScreenPanel;
     [SerializeField] private AudioClip crackedScreenSoundEffect;
+    [SerializeField] private float fadeDuration = 2f;
+    private Image crackedScreenImage;
+    private Coroutine fadeCoroutine;
 
     private void Start()
     {
+        crackedScreenImage = crackedScreenPanel.GetComponentInChildren<Image>();
         crackedScreenPanel.SetActive(false);
     }
 
@@ -30,10 +36,49 @@ public class ScreenComponent : ProbeComponent
     private void ShowCrackedScreen()
     {
         crackedScreenPanel.SetActive(true);
+        SetCrackedScreenAlphaToFull();
+        FadeCrackedScreen();
     }
     
     private void HideCrackedScreen()
     {
         crackedScreenPanel.SetActive(false);
+    }
+
+    private void SetCrackedScreenAlphaToFull()
+    {
+        Color c = crackedScreenImage.color;
+        c.a = 1f;
+        crackedScreenImage.color = c;
+    }
+    
+    private void FadeCrackedScreen()
+    {
+        if (fadeCoroutine != null)
+            StopCoroutine(fadeCoroutine);
+
+        fadeCoroutine = StartCoroutine(FadeOutRoutine());
+    }
+
+    //slowly fades out the 
+    private IEnumerator FadeOutRoutine()
+    {
+        float elapsed = 0f;
+        Color crackedScreenImageColor = crackedScreenImage.color;
+
+        while (elapsed < fadeDuration)
+        {
+            //Slowly fade the alpha to 0
+            elapsed += Time.deltaTime;
+            crackedScreenImageColor.a = 1f - Mathf.Clamp01(elapsed / fadeDuration);
+            crackedScreenImage.color = crackedScreenImageColor;
+            yield return null;
+        }
+
+        crackedScreenImageColor.a = 0f;
+        crackedScreenImage.color = crackedScreenImageColor;
+        HideCrackedScreen();
+        fadeCoroutine = null;
+        RepairComponent();
     }
 }

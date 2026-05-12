07a3250bf0a1684ebf4054b19b37505b60c85e54
Blake Anderson <blanderson1919@outlook.com>
2026-05-11 17:49:16 -0700

    Added feature where you can pay coins to unlock different starting check points

--- Assets/Scripts/CheckpointChooser.cs
+++ Assets/Scripts/CheckpointChooser.cs
@@ -1,68 +1,89 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class CheckpointChooser : MonoBehaviour
 {
-
-    [SerializeField] private List<CheckpointEntry> selectableStages = new  List<CheckpointEntry>();
+    [SerializeField] private List<CheckpointEntry> selectableStages = new List<CheckpointEntry>();
 
     [SerializeField] private Button leftButton;
     [SerializeField] private Button rightButton;
     [SerializeField] private TextMeshProUGUI displayName;
     [SerializeField] private Image lockSprite;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private TextMeshProUGUI buyButtonText;
+    [SerializeField] private Button startGameButton;
 
     [SerializeField] GameObject checkpointSpawner;
 
     private CheckpointSpawnScript checkpointSpawnScript;
-
     private int index = 0;
 
     void Start()
     {
         leftButton.onClick.AddListener(OnLeftButtonPressed);
         rightButton.onClick.AddListener(OnRightButtonPressed);
+        buyButton.onClick.AddListener(OnBuyButtonPressed);
 
         checkpointSpawnScript = checkpointSpawner.GetComponent<CheckpointSpawnScript>();
+        UpdateDisplay();
+    }
+
+    void OnEnable()
+    {
+        if (checkpointSpawnScript != null)
+            UpdateDisplay();
     }
 
     void OnDestroy()
     {
         leftButton.onClick.RemoveListener(OnLeftButtonPressed);
         rightButton.onClick.RemoveListener(OnRightButtonPressed);
+        buyButton.onClick.RemoveListener(OnBuyButtonPressed);
     }
 
-    private void OnLeftButtonPressed(){
+    private void OnLeftButtonPressed()
+    {
         index = index > 0 ? (index - 1) % selectableStages.Count : selectableStages.Count - 1;
-
         GameStateManager.Instance.startingGameStage = selectableStages[index].stage;
         GameStateManager.Instance.startingScore = selectableStages[index].startingScore;
-        SetDisplayText(selectableStages[index].displayName);
-        SetLockSprite(selectableStages[index].isLocked);
         checkpointSpawnScript.DecrementStartingCheckpoint();
+        UpdateDisplay();
     }
 
-    private void OnRightButtonPressed(){
+    private void OnRightButtonPressed()
+    {
         index = (index + 1) % selectableStages.Count;
-
         GameStateManager.Instance.startingGameStage = selectableStages[index].stage;
         GameStateManager.Instance.startingScore = selectableStages[index].startingScore;
-        SetDisplayText(selectableStages[index].displayName);
-        SetLockSprite(selectableStages[index].isLocked);
         checkpointSpawnScript.IncrementStartingCheckpoint();
+        UpdateDisplay();
     }
 
-    private void SetDisplayText(string displayText)
+    private void OnBuyButtonPressed()
     {
-        displayName.text = displayText;
+        if (GameStateManager.Instance.TryUnlockStage(selectableStages[index].stage))
+            UpdateDisplay();
     }
 
-    private void SetLockSprite(bool isLocked)
+    private void UpdateDisplay()
     {
-        lockSprite.gameObject.SetActive(isLocked);
+        CheckpointEntry entry = selectableStages[index];
+        bool locked = entry.unlockCost > 0 && !GameStateManager.Instance.IsStageUnlocked(entry.stage);
+
+        displayName.text = entry.displayName;
+        lockSprite.gameObject.SetActive(locked);
+        buyButton.gameObject.SetActive(locked);
+
+        if (locked)
+        {
+            buyButtonText.text = "Unlock: " + entry.unlockCost + " coins";
+            buyButton.interactable = GameStateManager.Instance.GetCoins() >= entry.unlockCost;
+        }
+
+        if (startGameButton != null)
+            startGameButton.interactable = !locked;
     }
 }
 
@@ -72,5 +93,5 @@ public class CheckpointEntry
     public GameStateManager.GameStage stage;
     public float startingScore;
     public string displayName;
-    public bool isLocked;
+    public int unlockCost;
 }

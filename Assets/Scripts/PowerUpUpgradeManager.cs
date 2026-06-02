5808e3889cf24fbb8914d8c25007f5e67f312a32
ashwathsrinivasan2 <asrinivasan@seattleu.edu>
2026-06-02 04:49:48 -0700

    adjusted power up upgrade costs to scale exponentially

--- Assets/Scripts/PowerUpUpgradeManager.cs
+++ Assets/Scripts/PowerUpUpgradeManager.cs
@@ -12,6 +12,22 @@ public class PowerUpUpgradeManager : MonoBehaviour
     public const int MaxLevel = 5;
 
     private int[] _levels = new int[5];
+    private int[,] upgradeCosts = {
+//level  0   1   2   3   4
+        {5, 10, 20, 40, 100},   //hyperspace
+        {5, 10, 20, 40, 100},   //force field
+        {5, 10, 20, 40, 100},   //R.U.M.
+        {5, 10, 20, 40, 100},   //shockwave
+        {5, 10, 20, 40, 100}    //laser drill
+    };
+    private int[,] durations = {
+//level  0   1   2   3   4   5
+        {5, 10, 15, 20, 25, 30},    //hyperspace
+        {5, 10, 15, 20, 25, 30},    //force field
+        {5, 10, 15, 20, 25, 30},    //R.U.M.
+        {5, 10, 15, 20, 25, 30},    //shockwave
+        {5, 10, 15, 20, 25, 30}     //laser drill
+    };
 
     private void Awake()
     {
@@ -22,8 +38,8 @@ public class PowerUpUpgradeManager : MonoBehaviour
 
     public int GetLevel(int i) => _levels[i];
     public bool IsMaxed(int i) => _levels[i] >= MaxLevel;
-    public int GetUpgradeCost(int i) => 3 + (_levels[i] * 3);
-    public float GetDuration(int i) => (_levels[i] + 1f) * 5f;
+    public int GetUpgradeCost(int i) => i <= 4 ? upgradeCosts[i, _levels[i]] : 99999999;
+    public float GetDuration(int i) => i <= 5 ? durations[i, _levels[i]] : 0;
 
     public bool TryUpgrade(int i)
     {
